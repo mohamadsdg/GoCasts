96e344d688c851ba2cf7bcde51923abda19b74a1
mohamadsdg <mohamad.r.sadeghi93@gmail.com>
2020-11-05 22:28:36 +0330

    work on loop | slice array

--- code/cards/main.go
+++ code/cards/main.go
@@ -6,8 +6,12 @@ func main(){
 	// var card string = "Ace of Spedes"
 	// card := "Ace of Spedes" // declare and assing
 	//card = "ddfdf" // reassign
-	card := newCard() // declare and assing
-	fmt.Println(card)
+	// card := newCard() 
+	cards := []string{newCard(),"Ace of Diamonds"}
+	cards = append(cards,"Six of Spedes") //return new slice and reassign to prev variable
+	for i,card := range cards{
+		fmt.Println(i,card)
+	}
 }
 
 func newCard() string{
