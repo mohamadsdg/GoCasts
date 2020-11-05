6b83680e87013706913fb288358b6dc2306cc416
mohamadsdg <mohamad.r.sadeghi93@gmail.com>
2020-11-05 22:12:52 +0330

    work on function

--- code/cards/main.go
+++ code/cards/main.go
@@ -4,7 +4,12 @@ import "fmt"
 
 func main(){
 	// var card string = "Ace of Spedes"
-	card := "Ace of Spedes" // declare and assing
+	// card := "Ace of Spedes" // declare and assing
 	//card = "ddfdf" // reassign
+	card := newCard() // declare and assing
 	fmt.Println(card)
+}
+
+func newCard() string{
+	return "five of Diamond"
 }
