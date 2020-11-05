8636ed397a7b6de034c12560d0f03cda6b212d65
mohamadsdg <mohamad.r.sadeghi93@gmail.com>
2020-11-05 17:24:51 +0330

    variable declar

---
+++ code/cards/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main(){
+	// var card string = "Ace of Spedes"
+	card := "Ace of Spedes" // declare and assing
+	//card = "ddfdf" // reassign
+	fmt.Println(card)
+}
